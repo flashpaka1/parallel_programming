6f687d5f722937ece166130242053ef746d9a216
Daniel Billingsley <141796606+flashpaka1@users.noreply.github.com>
2023-08-20 17:19:26 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello World!");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
